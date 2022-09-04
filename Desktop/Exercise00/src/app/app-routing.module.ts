2d9b5923231303920af97b12868f09dc2f1e9f74
shomes333 <windruh@gmail.com>
2022-09-04 15:13:25 -0300

    commit

--- Desktop/Exercise00/src/app/app-routing.module.ts
+++ Desktop/Exercise00/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './vista/login/login.component';
+import { NuevoComponent } from './vista/nuevo/nuevo.component';
+import { EditarComponent } from './vista/editar/editar.component';
+import { DashboardComponent } from './vista/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'' , redirectTo:'login' , pathMatch :'full'},
+  { path:'login', component:LoginComponent },
+  { path:'dashboard', component:DashboardComponent },
+  { path:'nuevo', component:NuevoComponent },
+  { path:'editar', component:EditarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [LoginComponent,DashboardComponent,NuevoComponent,EditarComponent]
--- Desktop/Exercise00/src/app/app.module.ts
+++ Desktop/Exercise00/src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './plantillas/header/header.component';
+import { FooterComponent } from './plantillas/footer/footer.component';
+// import { VistaComponent } from './vista/vista.component';
+// import { LoginComponent } from './vista/login/login.component';
+// import { DashboardComponent } from './vista/dashboard/dashboard.component';
+// import { NuevoComponent } from './vista/nuevo/nuevo.component';
+// import { EditarComponent } from './vista/editar/editar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    routingComponents
+    // VistaComponent,
+    // LoginComponent,
+    // DashboardComponent,
+    // NuevoComponent,
+    // EditarComponent
   ],
   imports: [
     BrowserModule,
